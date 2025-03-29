ed07e02726e16448027f065e3e3973053dbfa5c8
Courage Zeng <czeng@lcs.on.ca>
2025-03-29 14:05:38 -0400

    Initial Commit

---
+++ JokeFinder/JokeFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  JokeFinderApp.swift
+//  JokeFinder
+//
+//  Created by 曾梓恒 on 2025-03-29.
+//
+
+import SwiftUI
+
+@main
+struct JokeFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
