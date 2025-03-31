7b545146f0aac7b4ec356871a6f335566a875d7f
Courage Zeng <czeng@lcs.on.ca>
2025-03-31 19:11:10 -0400

    Added a tab view to switch between getting new jokes and reviewing favourites.

---
+++ JokeFinder/Views/FavouriteJokesView.swift
@@ -0,0 +1,23 @@
+//
+//  FavouriteJokesView.swift
+//  JokeFinder
+//
+//  Created by 曾梓恒 on 2025-03-31.
+//
+
+import SwiftUI
+ 
+struct FavouriteJokesView: View {
+    var body: some View {
+        NavigationStack {
+            VStack {
+                Text("This will show saved jokes.")
+            }
+            .navigationTitle("Favourites")
+        }
+    }
+}
+ 
+#Preview {
+    FavouriteJokesView()
+}
---
+++ JokeFinder/Views/LandingView.swift
@@ -0,0 +1,48 @@
+//
+//  LandingView.swift
+//  JokeFinder
+//
+//  Created by 曾梓恒 on 2025-03-31.
+//
+
+
+import SwiftUI
+ 
+struct LandingView: View {
+    
+    // MARK: Stored properties
+    @State var currentTab = 0
+    
+    // MARK: Computed properties
+    var body: some View {
+        TabView(selection: $currentTab) {
+            
+            JokeView()
+                .tabItem {
+                    Label {
+                        Text("New Jokes")
+                    } icon: {
+                        Image(systemName: "smiley")
+                    }
+                    
+                }
+                .tag(1)
+            
+            FavouriteJokesView()
+                .tabItem {
+                    Label {
+                        Text("Favourites")
+                    } icon: {
+                        Image(systemName: "heart.fill")
+                    }
+                    
+                }
+                .tag(2)
+            
+        }
+    }
+}
+ 
+#Preview {
+    LandingView()
+}
