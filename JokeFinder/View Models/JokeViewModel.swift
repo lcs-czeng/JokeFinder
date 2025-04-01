06e8b57e27af58017f5c03bc53aee7d3f75b629d
Courage Zeng <czeng@lcs.on.ca>
2025-03-31 20:06:10 -0400

    Saved jokes are now persisted to a JSON file kept on device.

--- JokeFinder/View Models/JokeViewModel.swift
+++ JokeFinder/View Models/JokeViewModel.swift
@@ -20,9 +20,7 @@ class JokeViewModel {
     var favouriteJokes: [Joke] = []
     
     // MARK: Initializer(s)
-    init(
-        currentJoke: Joke? = nil
-    ) {
+    init(currentJoke: Joke? = nil) {
         
         // Take whatever joke was provided when an instance of
         // this view model is created, and make it the current joke.
@@ -30,11 +28,15 @@ class JokeViewModel {
         // Otherwise, the default value for the current joke
         // will be a nil.
         self.currentJoke = currentJoke
-        
+ 
         // Load a joke from the endpoint
         Task {
             await self.fetchJoke()
         }
+        
+        // Get saved jokes from device storage
+        loadFavouriteJokes()
+ 
     }
     
     // Add the current joke to the list of favourites
@@ -47,7 +49,10 @@ class JokeViewModel {
         
         // How many saved jokes are there now?
         print("There are \(favouriteJokes.count) jokes saved.")
-     
+        
+        // Write the updated list of jokes to the JSON file stored on device
+        self.persistFavouriteJokes()
+ 
     }
     
     // Delete a joke from the list of favourites
@@ -60,7 +65,10 @@ class JokeViewModel {
         
         // How many saved jokes are there now?
         print("There are \(favouriteJokes.count) jokes saved.")
-     
+        
+        // Write the updated list of jokes to the JSON file stored on device
+        self.persistFavouriteJokes()
+ 
     }
     
     // MARK: Function(s)
@@ -77,12 +85,8 @@ class JokeViewModel {
         
         // 1. Attempt to create a URL from the address provided
         let endpoint = "https://official-joke-api.appspot.com/random_joke"
-        guard let url = URL(
-            string: endpoint
-        ) else {
-            print(
-                "Invalid address for JSON endpoint."
-            )
+        guard let url = URL(string: endpoint) else {
+            print("Invalid address for JSON endpoint.")
             return
         }
         
@@ -95,23 +99,11 @@ class JokeViewModel {
         do {
             
             // Fetch the data
-            let (
-                data,
-                _
-            ) = try await URLSession.shared.data(
-                from: url
-            )
-            
+            let (data, _) = try await URLSession.shared.data(from: url)
+ 
             // Print the received data in the debug console
-            print(
-                "Got data from endpoint, contents of response are:"
-            )
-            print(
-                String(
-                    data: data,
-                    encoding: .utf8
-                )!
-            )
+            print("Got data from endpoint, contents of response are:")
+            print(String(data: data, encoding: .utf8)!)
             
             // 3. Decode the data into a Swift data type
             
@@ -120,10 +112,7 @@ class JokeViewModel {
             
             // Use the decoder object to convert the raw data
             // into an instance of our Swift data type
-            let decodedData = try decoder.decode(
-                Joke.self,
-                from: data
-            )
+            let decodedData = try decoder.decode(Joke.self, from: data)
             
             // If we got here, decoding succeeded,
             // return the instance of our data type
@@ -132,18 +121,78 @@ class JokeViewModel {
         } catch {
             
             // Show an error that we wrote and understand
-            print(
-                "Count not retrieve data from endpoint, or could not decode into an instance of a Swift data type."
-            )
-            print(
-                "----"
-            )
+            print("Count not retrieve data from endpoint, or could not decode into an instance of a Swift data type.")
+            print("----")
             
             // Show the detailed error to help with debugging
-            print(
-                error
-            )
-                   
-               }
-           }
+            print(error)
+            
+        }
+    }
+    
+    // Load saved jokes from file on device
+    func loadFavouriteJokes() {
+        
+        // Get a URL that points to the saved JSON data containing our list of favourite jokes
+        let filename = getDocumentsDirectory().appendingPathComponent(fileLabel)
+        
+        print("Filename we are reading persisted jokes from is:")
+        print(filename)
+        
+        // Attempt to load from the JSON in the stored file
+        do {
+            
+            // Load the raw data
+            let data = try Data(contentsOf: filename)
+            
+            print("Got data from file, contents are:")
+            print(String(data: data, encoding: .utf8)!)
+            
+            // Decode the data into Swift native data structures
+            self.favouriteJokes = try JSONDecoder().decode([Joke].self, from: data)
+            
+        } catch {
+            
+            print(error)
+            print("Could not load data from file, initializing with empty list.")
+            
+            self.favouriteJokes = []
+        }
+        
+    }
+    
+    // Write favourite jokes to file on device
+    func persistFavouriteJokes() {
+        
+        // Get a URL that points to the saved JSON data containing our list of people
+        let filename = getDocumentsDirectory().appendingPathComponent(fileLabel)
+        
+        print("Filename we are writing persisted jokes to is is:")
+        print(filename)
+        
+        do {
+            
+            // Create an encoder
+            let encoder = JSONEncoder()
+            encoder.outputFormatting = .prettyPrinted
+            
+            // Encode the list of people we've tracked
+            let data = try encoder.encode(self.favouriteJokes)
+            
+            // Actually write the JSON file to the documents directory
+            try data.write(to: filename, options: [.atomicWrite, .completeFileProtection])
+            
+            print("Wrote data to file, contents are:")
+            print(String(data: data, encoding: .utf8)!)
+            
+            print("Saved data to documents directory successfully.")
+            
+        } catch {
+            
+            print(error)
+            print("Unable to write list of favourite jokes to documents directory.")
+        }
+        
+    }
 }
+ 
